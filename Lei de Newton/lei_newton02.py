eb8f2f96359abe457236c69938bd799416e0106d
Leo Gama J <53717882+LeoGamaJ@users.noreply.github.com>
2022-06-06 16:34:25 -0300

    Lei de Newton 02

---
+++ Lei de Newton/lei_newton02.py
@@ -0,0 +1,9 @@
+# Lei de Newton 1.0
+
+m = float(input('Insira o valor da Massa: '))
+a = float(input('Inisra do valor da Aceleração: '))
+f = m * a
+
+print('-=-'*20)
+print(f'Uma Massa com valor de {m}, multiplicada por uma Aceleração de {a}, possui uma força de: {f}.')
+print('-=-'*20)
